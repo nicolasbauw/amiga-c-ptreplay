91fa8cfba9587c1b6aef82430b2cec338604ca36
Nicolas BAUW <nbauw@hotmail.com>
2020-10-05 21:32:26 +0200

    Fixed for VBCC

--- include/ptreplay.h
+++ include/ptreplay.h
@@ -1,30 +1,7 @@
+#include <clib/ptreplay_protos.h>
 
-/* ptreplay.h -- definition of ptreplay.library structures */
-
-#ifndef	LIBRARIES_PTREPLAY_H 
-#define LIBRARIES_PTREPLAY_H
-
-#ifndef	EXEC_TYPES_H
-#include <exec/types.h>
+#ifndef __NOLIBBASE__
+extern struct Library *PTReplayBase;
 #endif
 
-#define PTREPLAYNAME "ptreplay.library"
-
-struct Module
-{
-    STRPTR mod_Name;
-/* The rest is private for now, but more details may be released later. */
-};
-
-/* This structure is returned by GetSample function */
-struct PTSample
-{
-	UBYTE Name[22];		/* Null terminated string with samplename */
-	UWORD Length;		/* Sample length in words */
-	UBYTE FineTune;		/* FineTune of sample in lower 4 bits */
-	UBYTE Volume;		/* Volume of sample */
-	UWORD Repeat;		/* Repeat start in number of words */
-	UWORD Replen;		/* Repeat length in number of words */
-};
-
-#endif /* LIBRARIES_PTREPLAY_H */
+#include <inline/ptreplay_protos.h>
